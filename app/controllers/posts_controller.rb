25542631b11f764e10080b7a013e159c3a271351
1motiur4 <92241810+1motiur4@users.noreply.github.com>
2024-06-23 19:17:26 -0400

    no scaffold pt2 done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController 
     def index
+        @posts = Post.all
     end
 
     def new 
+        @post = Post.new
     end
 
     def create
@@ -11,4 +13,24 @@ class PostsController < ApplicationController
 
         redirect_to "/posts"
     end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def edit 
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        my_params = params.require(:post).permit(:title, :content)
+        @post = Post.find(params[:id])
+        @post.update(my_params)
+
+        redirect_to "/posts"
+    end
+
+    def destroy
+    end
+
 end
